542e3390b0fbc5ea6a6e3fd98793d594d2d066e5
Nicole Sims <nssims105@gmail.com>
2020-03-24 19:34:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  def name= (name_of_dog)
+    @name = name_of_dog
+  end 
+  
+  def name 
+    @name 
+  end
+end 
+
+class Dog 
+  def breed= (breed_of_dog)
+      @breed = breed_of_dog
+  end
+
+  def breed
+  @breed
+  end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def name=(name)
+    @name = (name)
+  end
+
+  def name
+    @name
+  end
+end
+
+class Person 
+  def job=(job_of_person)
+    @job = job_of_person
+  end 
+
+  def job
+    @job 
+  end
+end
+
+
+
+
