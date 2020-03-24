b8e97b19aa188c9fd24e4dccf195c1f76c41c220
Nicole Sims <nssims105@gmail.com>
2020-03-24 16:42:39 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def name= (name_of_dog)
+    @name = name_of_dog
+  end 
+  
+  def name 
+    @name 
+  end
+end 
+
+def breed= (breed_of_dog)
+  @breed = breed_of_dog
+end
+
+def breed
+  @breed
+end
+
